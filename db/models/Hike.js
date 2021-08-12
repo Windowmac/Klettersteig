009c734bb50303eeefd3221d981394063ddcf57c
McKendree Strommer <mckendree.strommer@gmail.com>
2021-08-12 10:42:53 -0700

    started building hike database instead of fetch request, at least as a demo, if not more

---
+++ db/models/Hike.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../connection');
+
+class Hike extends Model {}
+
+Hike.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    discovered_by: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+        unique: true,
+      },
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    latitude: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    longitude: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  { sequelize, modelName: 'hike', freezeTableName: true },
+);
+
+module.exports = Hike;
