ccda62e554b865e32efe25779b5a9f4e782fd289
McKendree Strommer <mckendree.strommer@gmail.com>
2021-08-12 00:29:50 -0700

    connected js then started tryng the fetch request, running into CORS blocker

--- public/js/index.js
+++ public/js/index.js
@@ -2,27 +2,53 @@ const userName = document.getElementById('user_name');
 const password = document.getElementById('password');
 const submitBtn = document.getElementById('submit_btn');
 
-const createUser = (userName, password) => {
-  console.log(userName);
-  if (userName.length && password.length) {
-    const body = {};
-    body.user_name = userName.value;
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log(password.value);
+  console.log(userName.value);
+  const body = {};
+  if (userName.value.length && password.value.length) {
+    body.username = userName.value;
     body.password = password.value;
   } else {
     window.alert('enter username and password');
     return;
   }
 
-  fetch('/api/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  }).then((res) => 
-    res.json()).then((result) => {
-      console.log(result);
-    });
-  };
+  try {
+    fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  } catch (err) {
+    throw new Error(err);
+  }
+});
 
-submitBtn.addEventListener('click', createUser(userName, password));
+const getApiInfo = async () => {
+  const apiInfo = await axios
+    .get('https://ridb.recreation.gov/api/v1/activities?limit=50&offset=0', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        apikey: 'a46a852b-c314-48ad-ad97-a1c064891d4b',
+      },
+      withCredentials: true,
+    })
+    .then((res) => res.json())
+    .then((response) => {
+      return response;
+    });
+
+  console.log(apiInfo);
+};
+
+getApiInfo();
--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const sequalize = require('./db/connection');
 const exphbs = require('express-handlebars');
 const { urlencoded } = require('express');
 const hbs = exphbs.create({});
+const axios = require('axios');
+const path = require('path');
 
 const PORT = process.env.PORT || 3030;
 const app = express();
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
