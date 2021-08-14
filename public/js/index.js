316d9de1ec14044e623b0ae9b19ccd9e4de35e86
Andy <awilliamscomposing@gmail.com>
2021-08-14 10:55:24 -0700

    Manually merging with main

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,50 @@
-// const userName = document.getElementById('user_name');
-// const password = document.getElementById('password');
-// const submitBtn = document.getElementById('submit_btn');
+const userName = document.getElementById('user_name');
+const password = document.getElementById('password');
+const submitBtn = document.getElementById('submit_btn');
+// const express = require('express');
+// const router = require('./routes');
+// const sequelize = require('./db/connection');
+// const exphbs = require('express-handlebars');
+// const { urlencoded } = require('express');
+// const hbs = exphbs.create({});
 
-// const createUser = (userName, password) => {
-//   console.log(userName);
-//   if (userName.length && password.length) {
-//     const body = {};
-//     body.user_name = userName.value;
-//     body.password = password.value;
-//   } else {
-//     window.alert('enter username and password');
-//     return;
-//   }
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log(password.value);
+  console.log(userName.value);
+  const body = {};
+  if (userName.value.length && password.value.length) {
+    body.username = userName.value;
+    body.password = password.value;
+  } else {
+    window.alert('enter username and password');
+    return;
+  }
 
-//   fetch('/api/users', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(body),
-//   }).then((res) => 
-//     res.json()).then((result) => {
-//       console.log(result);
-//     });
-//   };
+  try {
+    fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 
 const testLat = '45.52345';
 const testLong = '-122.67621';
@@ -70,5 +91,3 @@ const sunsetAndRise = () => {
 // Allow user to enter their preferred day to call from? API can take another parameter
 // Example: https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
 sunsetAndRise();
-
-submitBtn.addEventListener('click', createUser(userName, password));
