8b00e407f6a9cd8a4e8c6ac8c04766b998aa77be
McKendree Strommer <mckendree.strommer@gmail.com>
2021-08-14 16:14:46 -0700

    translating map html to javaScript

--- public/js/index.js
+++ public/js/index.js
@@ -48,13 +48,18 @@ function closeForm() {
 
 
 
-const buildMap = () => {
+const buildMap = (westBorder, southBorder, eastBorder, northBorder) => {
   const mapEl = document.createElement('iframe');
   mapEl.width = "425";
   mapEl.height = "350";
   mapEl.frameborder = "0";
   mapEl.scrolling = "no";
-  
+  mapEl.marginheight= "0";
+  mapEl.marginwidth= "0";
+  mapEl.src= "https://www.openstreetmap.org/export/embed.html?bbox=-123.15220643766226%2C45.34718457473261%2C-122.2691803146154%2C45.667670951364165&amp;layer=mapnik";
+  mapEl.style= "border: 1px, solid, black";
+
+
 
   const map = <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-123.15220643766226%2C45.34718457473261%2C-122.2691803146154%2C45.667670951364165&amp;layer=mapnik" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=11/45.5077/-122.7107">View Larger Map</a></small>
 
