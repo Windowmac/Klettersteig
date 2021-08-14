b2f7f387170eb6719673b353f4f041311f426df4
McKendree Strommer <mckendree.strommer@gmail.com>
2021-08-14 16:04:28 -0700

    testing iframe map

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 const userName = document.getElementById('user_name');
 const password = document.getElementById('password');
-const submitBtn = document.getElementById('submit_btn');
+const submitBtn = document.getElementById('create-btn');
 // const express = require('express');
 // const router = require('./routes');
 // const sequelize = require('./db/connection');
@@ -45,3 +45,17 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+
+
+const buildMap = () => {
+  const mapEl = document.createElement('iframe');
+  mapEl.width = "425";
+  mapEl.height = "350";
+  mapEl.frameborder = "0";
+  mapEl.scrolling = "no";
+  
+
+  const map = <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-123.15220643766226%2C45.34718457473261%2C-122.2691803146154%2C45.667670951364165&amp;layer=mapnik" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=11/45.5077/-122.7107">View Larger Map</a></small>
+
+getMapApi();
