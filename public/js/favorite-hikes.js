95acfae87dc11e20ce1ff4ff1f207a1c166852d0
Andy <awilliamscomposing@gmail.com>
2021-08-21 10:58:29 -0700

    More work on fetch post

--- public/js/favorite-hikes.js
+++ public/js/favorite-hikes.js
@@ -1,5 +1,5 @@
-// addtoFavs function should fetch this url: "localhost:3030/api/favorite-Hikes" as a POST
 // fetch should include a JSON object that you will define as your body (req.body) (see apiFavoriteHikesRoutes)
+// req.body.user_id, req.body.hike_id
 
 // on page load query our db for the favoriteHikes rows, probably only need the name of the hike to be displayed
 // you will likely use a join to get this information
@@ -10,20 +10,40 @@
 // You will add definitions like so ({where: {user_id=1 (get this from session token)}})
 // Hikes.findOne({where: {id: = hike_Id}})
 // Can use dummy tokens/accounts to test with before implementing session tokens
+
 // Databases are more useful for storing data than local storage
 // Local storage can be wiped out more easily and doesn't work between devices
 
-// POST,'localhost:3030/api/favorite-hikes/'
-// req.body.user_id, req.body.hike_id
+// fetch(url)
+//     .then(res => res.json())
+//     .then(data => {
+//         // iterate over users
+//         data.map((user) => {
+//             // create the elements
+//             let li = createNode('li'),
+//                 img = createNode('img'),
+//                 span = createNode('span');
+//             img.src = user.avatar_url;
+//             span.innerText = user.login;
+//             // append all elements
+//             appendNode(li, img);
+//             appendNode(li, span);
+//             appendNode(ul, li);
+//         });
+//     }).catch(err => {
+//         console.error('Error: ', err);
+//     });
 
 const addToFavs = () => {
     fetch("http://localhost:3030/api/favorite-hikes", {
         method: "POST",
         // Adding body or contents to send
         body: JSON.stringify({
-            title: "Hike",
-            body: "Test",
-            user_id: 1
+            user
+            // title: "Hike",
+            // body: "Test",
+            // user_id: 1
+            // hike_id: 1
         }),
         // Adding headers to the request
         headers: {
@@ -34,8 +54,12 @@ const addToFavs = () => {
     .then(response => response.json())
     // Displaying results to console
     .then(json => console.log(json))
+    .then(data => {
+        data.map((user) => {
+            console.log(`Data is ${data}`);
+        }
+    )
     .catch(err => {
-    // Handle error 
     console.log('Error: ', error);
     }
 );
