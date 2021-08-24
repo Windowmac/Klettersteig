22a204a3fbc1699601b87d07c2a72649053c0f69
Andy <awilliamscomposing@gmail.com>
2021-08-23 18:12:45 -0700

    Saving to pull from main

--- public/js/userRating.js
+++ public/js/userRating.js
@@ -4,7 +4,7 @@ const post = document.querySelector(".post");
 const widget = document.querySelector(".star-widget");
 const editBtn = document.querySelector(".edit");
 
-btn.onclick = ()=>{
+btn.onclick = () => {
   widget.style.display = "none";
   post.style.display = "block";
   editBtn.onclick = ()=>{
